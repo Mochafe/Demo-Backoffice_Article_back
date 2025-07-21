1fedfacc110d4077288e898baaa0958cf0c6fac8
Florian PASQUET <florianpasquett@gmail.com>
2025-07-21 18:26:40 +0200

    Add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user/user.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/demo-backoffice'),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
