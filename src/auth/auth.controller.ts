5120a5953965468b1243d3b26fcf8370c7cf3b19
Florian PASQUET <florianpasquett@gmail.com>
2025-07-23 11:14:35 +0200

    Add controller (Auth)

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,26 @@
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('register')
+  @HttpCode(HttpStatus.CREATED)
+  async register(@Body() registerDto: RegisterDto) {
+    return this.authService.register(
+      registerDto.email,
+      registerDto.password,
+      registerDto.firstname,
+      registerDto.lastname,
+    );
+  }
+
+  @Post('login')
+  @HttpCode(HttpStatus.ACCEPTED)
+  async login(@Body() loginDto: LoginDto) {
+    return this.authService.signIn(loginDto.email, loginDto.password);
+  }
+}
