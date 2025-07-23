717718dc40fbc1bd2721330c6a4fbd8094dcace2
Florian PASQUET <florianpasquett@gmail.com>
2025-07-23 11:21:44 +0200

    Add imports (MongooseModule, UsersModule, AuthModule, ArticlesModule)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserModule } from './user/user/user.module';
-import { UserModule } from './user/user.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ArticlesModule } from './article/articles.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/demo-backoffice'),
-    UserModule,
+    UsersModule,
+    AuthModule,
+    ArticlesModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
