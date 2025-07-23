4870856372694ec37e76da2d37acca124f2baf4a
Florian PASQUET <florianpasquett@gmail.com>
2025-07-23 11:10:07 +0200

    Add mongoose schema (Article)

---
+++ src/article/article.schema.ts
@@ -0,0 +1,56 @@
+import { Schema, SchemaFactory, Prop } from '@nestjs/mongoose';
+import mongoose, { Document, HydratedDocument } from 'mongoose';
+import { constants } from './constants';
+import { User } from 'src/users/user.schema';
+import { ArticleStatusEnum } from './articleStatus.enum';
+
+export type ArticleDocument = HydratedDocument<Article>;
+
+@Schema({ timestamps: true })
+export class Article extends Document {
+  id: string;
+
+  @Prop({
+    type: String,
+    required: true,
+    minlength: constants.title.min,
+    maxlength: constants.title.max,
+  })
+  title: string;
+
+  @Prop({
+    type: String,
+    required: true,
+    minlength: constants.content.min,
+    maxlength: constants.content.max,
+  })
+  content: string;
+
+  createdAt: Date;
+
+  updatedAt: Date;
+
+  @Prop({
+    type: String,
+    required: true,
+    enum: [ArticleStatusEnum.draft, ArticleStatusEnum.published],
+  })
+  status: ArticleStatusEnum;
+
+  @Prop({
+    type: Array<string>,
+    required: true,
+    minlength: constants.tags.min,
+    maxlength: constants.tags.max,
+  })
+  tags: string[];
+
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  })
+  author: User;
+}
+
+export const ArticleSchema = SchemaFactory.createForClass(Article);
+ArticleSchema.loadClass(Article);
